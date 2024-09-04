e2dee679bceb915afa9e834d9fd1e4599fda6133
Gabriel Alfonso <gabalfa@gmail.com>
2024-09-03 20:30:59 -0500

    Update header.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,7 +5,7 @@ import SideNav from "@/app/components/side-nav";
 
 function Header() {
   return (
-    <header className="h-screen bg-cover bg-no-repeat bg-center bg-[url('/bg-image-4.webp')]">
+    <header className="h-screen bg-cover bg-no-repeat bg-center bg-[url('/bg-image-9.webp')]">
       <div className="h-24 border-b p-4 flex flex-wrap md:flex-row items-center justify-between">
         <span className="text-4xl md:text-6xl text-white">Artesanal</span>
       </div>
