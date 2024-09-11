a7502e2ccea8038713a113201edaeb493431d065
Gabriel Alfonso <gabrielalfonso@Gabriels-MacBook-Pro.local>
2024-09-10 21:58:19 -0500

    refactor (head hero) font-size

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -6,15 +6,14 @@ import SideNav from "@/app/components/side-nav";
 function Header() {
   return (
     <section className=" h-screen bg-cover bg-no-repeat bg-center bg-[url('/home-image.png')]">
-      <div className="h-24 border-b flex flex-wrap md:flex-row items-center justify-between">
-        <SideNav />
-      </div>
+      <div className="h-24 border-b p-8 flex flex-wrap md:flex-row items-center justify-between"></div>
+      <SideNav />
 
-      <div className="flex items-center justify-center w-full md:h-full p-4 md:px-16">
-        <p className="text-6xl md:text-8xl md:w-2/3 text-white mx-auto self-center">
+      <div className="flex items-center justify-center w-full md:h-full p-8">
+        <p className="text-6xl md:text-8xl md:w-1/2 text-white mx-auto self-center">
           Bienvenida al destino para los amantes de la artesanía
         </p>
-        <p className="text-6xl w-1/3 text-white mx-auto self-center"></p>
+        <p className="text-6xl w-1/2 text-white mx-auto self-center"></p>
       </div>
     </section>
   );
